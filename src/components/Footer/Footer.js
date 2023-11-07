2d52ec83714bb31ecb2b02777966eb20a55db0cc
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2023-11-07 22:02:24 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className="text2">
+    <hr></hr>
+    <h1 class="text-white header-title ls-2">Create with us!</h1>
+    <br></br>
+    <p class="con">A taste which cannot be found anywhere else.</p>
+    <div class="icons">
+    <div class="icons2">
+    <i class="ri-instagram-fill ri-xl"></i>
+    <i class="ri-facebook-box-fill ri-xl"></i>
+    <i class="ri-pinterest-fill ri-xl"></i>
+    </div>
+    </div>
+    </div>
+    </footer>
+  )
+}
+
+export default Footer
